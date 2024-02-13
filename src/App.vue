71de7cee27ffc0c7410e4c0d245868babc6accaa
Mudden88 <mudden@live.se>
2024-02-13 12:39:02 +0100

    new file:   src/assets/Vectorqmark.png 	new file:   src/components/Hero.vue 	deleted:    src/components/StartQuiz.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterLink } from "vue-router";
+import Hero from './components/Hero.vue'
 </script>
 
 <template>
   <RouterLink to="/">Home</RouterLink>
   <RouterLink to="/page">Page</RouterLink>
+  <Hero />
   <RouterView></RouterView>
 </template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="startQuiz">
+      <img class="qm1" src="../assets/Vectorqmark.png">
+      <img class="qm2" src="../assets/Vectorqmark.png">
+      <img class="qm2" src="../assets/Vectorqmark.png">
+      <h2>Welcome</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 20px;
+}
+
+.container {
+  width: 100vw;
+}
+
+.startQuiz {
+  background: var(--Main-lighter-color);
+  color: var(--Light-color);
+  width: 338px;
+  height: 214px;
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.startQuiz h2 {
+  font-size: 40px;
+}
+</style>
