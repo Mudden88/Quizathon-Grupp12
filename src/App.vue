548ac203cbf4cb2c8b22a51628a09d1444266722
ViktorT <v.tolergard@gmail.com>
2024-02-14 18:39:27 +0100

    Gjort komponent för header-rubrik, komponent och en komponent för Grid-nät för kategorierna

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { RouterLink } from "vue-router";
-import Hero from './components/Hero.vue'
+import Hero from "./components/Hero.vue";
+import CategoriesGrid from "./components/CategoriesGrid.vue";
+import SectionHeader from "./components/SectionHeader.vue";
 </script>
 
 <template>
   <RouterLink to="/">Home</RouterLink>
   <RouterLink to="/page">Page</RouterLink>
   <Hero />
+  <CategoriesGrid />
   <RouterView></RouterView>
 </template>
