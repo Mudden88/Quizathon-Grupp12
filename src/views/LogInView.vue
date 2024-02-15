57c20f935fa0d2b76f7c3b427d242b585a919ac1
EllenSuni <ellen.suni@outlook.com>
2024-02-15 14:19:41 +0100

    made inputfield component

--- src/views/LogInView.vue
+++ src/views/LogInView.vue
@@ -1,3 +1,18 @@
+<script setup>
+import InputField from "../components/InputField.vue";
+</script>
+
 <template>
   <h1>Log in</h1>
+
+  <form action="#">
+    <InputField
+      label-prop="Username"
+      placeholder-prop="Username"
+      id-prop="username" />
+    <InputField
+      label-prop="Password"
+      placeholder-prop="Password"
+      id-prop="password" />
+  </form>
 </template>
