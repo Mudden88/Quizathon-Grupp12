bc463fb08bd15afa123a45ef4583b6b132b42d3c
ViktorT <v.tolergard@gmail.com>
2024-02-16 13:06:41 +0100

    testar en grej i router

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import HomeView from "./views/HomeView.vue";
 import PageView from "./views/PageView.vue";
+import AllCategories from "./components/AllCategories.vue";
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -13,6 +14,11 @@ export default createRouter({
     {
       component: PageView,
       path: "/page",
+    },
+    {
+      component: AllCategories,
+      path: "/categoryId",
+      props: true,
     },
   ],
 });
