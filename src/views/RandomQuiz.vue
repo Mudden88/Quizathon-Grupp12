161ebed5e0bf0e8eda265fea9544b02d38d892ca
Mudden88 <mudden@live.se>
2024-02-18 21:56:47 +0100

    moved: RandomQuiz.vue to src/views/RandomQuiz.vue

---
+++ src/views/RandomQuiz.vue
@@ -0,0 +1,195 @@
+<script setup>
+import axios from 'axios'
+import { ref, computed } from 'vue'
+import router from '../router';
+
+//Variabler och referens värden
+const apiUrl = 'https://opentdb.com/api.php?amount=10'
+const questions = ref([])
+const currentIndex = ref(0)
+const selectedAnswerIndex = ref(null)
+const currentScore = ref(0)
+
+
+//Computed på shuffledAnswers, blandar svaren endast en gång per frågeomgång
+const shuffledAnswers = computed(() => {
+  if (questions.value.length > 0 && currentIndex.value < questions.value.length) {
+    const question = questions.value[currentIndex.value]
+    const answers = [...question.incorrect_answers, question.correct_answer]
+    shuffleArray(answers)
+
+    return answers
+  }
+  return []
+})
+
+//function för att hämta quiz och lagra i questions arrayen
+async function fetchData() {
+  let response = await axios.get(apiUrl)
+
+  questions.value = response.data.results
+
+  return questions
+}
+//function för att blanda array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+//Öka siffra efter varje fråga
+function newIndex() {
+  currentIndex.value += 1
+}
+
+// tar emot index från svaren och tilldelar index som värde för att kunna hantera vilket svar som är klickat på.
+function handleAnswerOnClick(index) {
+  selectedAnswerIndex.value = index
+}
+
+
+//Logik för knappen, är svaret rätt. ökas currentScore,
+//selectedAnswerIndex.value som fått ett index från functionen innan får
+// Null för att släcka Pop-color
+//Om currentIndex är 10 skickas man tillbaka till home,
+//annars får den ett nytt index och ny fråga dyker upp.
+function handleConfirmClick() {
+
+  const question = questions.value[currentIndex.value]
+  const selectedAnswer = shuffledAnswers.value[selectedAnswerIndex.value]
+
+  if (selectedAnswer === question.correct_answer) {
+    currentScore.value += 1
+  }
+
+  selectedAnswerIndex.value = null
+
+  if (currentIndex.value === 10) {
+    router.push("/")
+  } else {
+    newIndex()
+  }
+}
+//obvious
+fetchData()
+
+</script>
+
+<template>
+  <div class="container">
+    <!-- v-if för att kolla så att questions arrayen inte är tom. Är den tom så
+     går v-else igenom som en "laddfunktion"
+    for loop hämtar frågor och index, index för att hålla koll på vilken
+    fråga av 10st som visas. Använder v-html för "encode" ska fungera-->
+    <ul v-if="questions.length > 0">
+      <li v-for="(question, index) in questions" :key="question.question">
+        <div class="checkIndex" v-if="index === currentIndex">
+          <p>Question: {{ index + 1 }}/10
+            <span class="difficulty" v-html="question.difficulty"></span>
+          </p>
+          <p class="category">Category: <span v-html="question.category"></span></p>
+          <p class="currentScore">Score: {{ currentScore }} /10</p>
+          <hr>
+          <p class="mainQuestion" v-html="question.question"></p>
+          <!-- Hämtar blandade svar och svarsIndex,
+            ändrar färg med dynamiska klasser -->
+          <div class="answerContainer">
+            <p id="answer" v-for="(answer, answerIndex) in shuffledAnswers" :key="answer"
+              :class="{ selected: answerIndex === selectedAnswerIndex }" @click="() => handleAnswerOnClick(answerIndex)"
+              v-html="answer"></p>
+
+            <div class="button" @click="handleConfirmClick">Confirm</div>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <p v-else>Loading question...</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 390px;
+  text-align: center;
+}
+
+ul {
+  list-style-type: none;
+
+}
+
+hr {
+  width: 338px;
+  margin-left: 0;
+  color: rgba(128, 128, 128, 0.2);
+}
+
+p {
+  font-size: 32px;
+  margin: 0;
+}
+
+h3 {
+  font-size: 30px;
+}
+
+#answer {
+  width: 321px;
+  min-height: 63px;
+  background-color: var(--Accent-color);
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin: 10px;
+  font-size: 36px;
+}
+
+#answer:hover {
+  background-color: var(--Pop-color);
+}
+
+#answer:active {
+  transform: scale(1.1);
+}
+
+#answer.selected {
+  background-color: var(--Pop-color);
+  box-shadow: 5px 5px 20px var(--Main-color);
+}
+
+.difficulty {
+  font-size: 20px;
+  color: var(--Accent-color);
+}
+
+.currentScore {
+  background-color: var(--Pop-color);
+  width: fit-content;
+  padding: 0 10px;
+  border-radius: 10px;
+  margin: auto;
+}
+
+.category {
+  font-size: 25px;
+}
+
+.button {
+  width: 291px;
+  height: 63px;
+  border-radius: 20px;
+  background-color: var(--Main-lighter-color);
+  color: var(--Light-color);
+  font-size: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 20px;
+  margin-top: .5em;
+  cursor: pointer;
+}
+</style>
