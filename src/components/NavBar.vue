51e48bbdf7c4c702c3cb1359bf15298df11edf04
Cecilia Rissanen <cissi@MacBook-Air-som-tillhor-Cecilia.local>
2024-02-20 13:25:58 +0100

    bara för att

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,11 @@
 <script setup>
 import { ref } from "vue";
 import { RouterLink } from "vue-router";
-import imgUrl from "/src/assets/quizathon-logo.png"
 import burger from "/src/assets/burger.png"
 import menuX from "/src/assets/menuX.png"
-import instructionIcon from "/src/assets/icons/instructions-icon.png"
-import profileIcon from "/src/assets/icons/profile-icon.png"
-import leaderboardIcon from "/src/assets/icons/crown-icon.png"
-import contactIcon from "/src/assets/icons/letter-icon.png"
-import profileOutline from "/src/assets/icons/profile-outline-icon.png"
 
-const icon = ref(burger)
-
-const ifMenuClicked = ref(false)
+const icon = ref(burger),
+ifMenuClicked = ref(false)
 
 function menuClicked() {
 ifMenuClicked.value = !ifMenuClicked.value
@@ -28,14 +21,14 @@ icon.value = ifMenuClicked.value ? menuX : burger
   <div class="burger" @click="closeMenu">
   <img id="burger" :src="icon" alt="" @click="menuClicked">
 </div>
-    <img id="logo" :src="imgUrl" alt="logo">
-    <img id="profile" :src="profileOutline" alt="">
+    <img id="logo" src="../assets/quizathon-logo.png" alt="logo">
+    <img id="profile" src="../assets/icons/profile-outline-icon.png" alt="">
   </div>
   <div class="burger-menu" v-show="ifMenuClicked">
-    <div @click="menuClicked" class="icons"><RouterLink to="/instructions">Instructions </RouterLink><img :src="instructionIcon" alt=""></div>
-    <div @click="menuClicked" class="icons"><RouterLink to="/profile">Profile</RouterLink><img :src="profileIcon" alt=""></div>
-    <div @click="menuClicked" class="icons"><RouterLink to="/leaderboard">Leaderboard</RouterLink><img :src="leaderboardIcon" alt=""></div>
-    <div @click="menuClicked" class="icons"><RouterLink to="/contact">Contact</RouterLink><img :src="contactIcon" alt=""></div>
+    <div @click="menuClicked" class="icons"><RouterLink to="/instructions">Instructions </RouterLink><img src="../assets/icons/instructions-icon.png" alt=""></div>
+    <div @click="menuClicked" class="icons"><RouterLink to="/profile">Profile</RouterLink><img src="../assets/icons/profile-icon.png" alt=""></div>
+    <div @click="menuClicked" class="icons"><RouterLink to="/leaderboard">Leaderboard</RouterLink><img src="../assets/icons/crown-icon.png" alt=""></div>
+    <div @click="menuClicked" class="icons"><RouterLink to="/contact">Contact<img src="../assets/icons/letter-icon.png" alt=""></RouterLink></div>
     </div>
     </nav>
 </header>
@@ -94,8 +87,13 @@ a {
 
 .icons {
     display: flex;
+    border-top: solid #3a5e57 1px;
     margin-top: 50px;
+    padding-top: 30px;
+    padding-right: 15px;
 }
+
+
 
 :root {
   --background: #eef1ef;
@@ -106,4 +104,23 @@ a {
   --bgText: #272727;
 }
 
+@media screen and (min-width: 768px) {
+ header {
+#logo {
+  height: 150px;
+}
+.navbar {
+  height: 160px;
+  width: 100vw;
+  background-color: #eef1ef;
+  display: flex;
+  justify-content: space-between;
+}
+.burger-menu {
+display: flex;
+flex-direction: column;
+}
+
+}
+}
 </style>
