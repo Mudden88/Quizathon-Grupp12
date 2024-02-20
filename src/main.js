095d6db274cdf414bc38e9dceb662dc6bbc80346
EllenSuni <ellen.suni@outlook.com>
2024-02-20 16:30:00 +0100

    user saves to localstorage when logging in

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-import { createApp } from "vue";
+import { createApp, watch } from "vue";
 import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
 const pinia = createPinia();
+
+watch(
+  pinia.state,
+  (state) => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  },
+  { deep: true }
+);
 
 createApp(App).use(router).use(pinia).mount("#app");
