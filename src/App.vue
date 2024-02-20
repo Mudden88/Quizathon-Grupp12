67de41c5892e0b976bab9a3bded3d76577e0daae
Cecilia Rissanen <cecilia.rissanen@iths.com>
2024-02-20 14:26:42 +0100

    ändrat HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <script setup>
-import { RouterLink } from "vue-router";
-import Hero from "./components/Hero.vue";
-
-import PopularCategories from "./components/PopularCategories.vue";
-import AllCategories from "./components/AllCategories.vue";
-
 import NavBar from "./components/NavBar.vue";
 
 import { useStorage } from "./storage";
@@ -14,10 +8,6 @@ const fromStorage = useStorage();
 
 <template>
   <NavBar />
-  <Hero />
-
-  <PopularCategories />
-  <AllCategories />
 
   <h1 v-if="fromStorage.user.isLoggedIn">
     Welcome {{ fromStorage.user.username }}!
