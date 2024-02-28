9f55303f6e36cdd19925e55aa43ebaebf35b5eec
EllenSuni <ellen.suni@outlook.com>
2024-02-28 13:53:04 +0100

    Created view and updated routes

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { usersRef, db } from "../firebase";
+import { onValue, set, ref as dbref } from "firebase/database";
+import { ref } from "vue";
+
+const data = ref(null);
+
+onValue(usersRef, (snapshot) => {
+  data.value = snapshot.val();
+  console.log(data.value);
+});
+</script>
