7080b036b9e32ce9cd41a8230887c3fa99f87ab9
EllenSuni <ellen.suni@outlook.com>
2024-03-05 11:39:55 +0100

    Removed console.log in leaderboard

--- src/views/LeaderboardView.vue
+++ src/views/LeaderboardView.vue
@@ -2,47 +2,50 @@
   <div>
     <h1>Leaderboard</h1>
     <ul class="leaderboard">
-      <li v-for="(user, index) in leaderboard" :key="index" class="leaderboard-item">
-      <div class="position">{{ index + 1 }}.</div>        <!--Position/Rankning-->
-      <div class="picture"></div>                         <!--Profilbild-->
-      <div class="username">{{ user.username }}</div>     <!--Användarnamn-->
-      <div class="score">{{ user.score }}p</div>          <!--Poäng-->
-    </li>
-  </ul>
+      <li
+        v-for="(user, index) in leaderboard"
+        :key="index"
+        class="leaderboard-item">
+        <div class="position">{{ index + 1 }}.</div>
+        <!--Position/Rankning-->
+        <div class="picture"></div>
+        <!--Profilbild-->
+        <div class="username">{{ user.username }}</div>
+        <!--Användarnamn-->
+        <div class="score">{{ user.score }}p</div>
+        <!--Poäng-->
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
 import { usersRef } from "../firebase";
 import { onValue } from "firebase/database";
 
-  export default {
+export default {
   data() {
     return {
       data: null,
-      leaderboard: []
+      leaderboard: [],
     };
   },
 
-  created(){                                    //Rendera leaderboard med data från Firebase i realtid
+  created() {
+    //Rendera leaderboard med data från Firebase i realtid
     onValue(usersRef, (snapshot) => {
-    this.data = snapshot.val();
-    console.log(this.data)
-    this.leaderboard = []
-    for (const key in this.data) {
-      console.log(key)
-      const user = this.data[key]
-      console.log(user)
+      this.data = snapshot.val();
+      this.leaderboard = [];
+      for (const key in this.data) {
+        const user = this.data[key];
         this.leaderboard.push({
           username: user.username,
-          score: user.totalscore
+          score: user.totalscore,
         });
-        this.leaderboard.sort((a, b) => b.score - a.score);         //Sorterar användare i ordning efter poäng
-    }
-})
+        this.leaderboard.sort((a, b) => b.score - a.score); //Sorterar användare i ordning efter poäng
+      }
+    });
   },
-
 };
 </script>
 
@@ -53,11 +56,11 @@ h1 {
   font-size: 24px;
 }
 
-.score{
+.score {
   margin-left: auto;
 }
 
-.position{
+.position {
   width: 50px;
 }
 .leaderboard {
@@ -66,7 +69,6 @@ h1 {
   width: 300px;
   flex-direction: column;
   padding: 10px;
-
 }
 
 .leaderboard-item {
